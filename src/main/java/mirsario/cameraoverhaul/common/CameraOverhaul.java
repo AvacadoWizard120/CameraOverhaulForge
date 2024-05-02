fb8f376bf6ba743c7a015451e718013b8bfe30d9
AvacadoWizard120 <edenjudeurias@gmail.com>
2024-05-02 00:10:37 -0500

    what the fuck is happening

--- src/main/java/mirsario/cameraoverhaul/common/CameraOverhaul.java
+++ src/main/java/mirsario/cameraoverhaul/common/CameraOverhaul.java
@@ -3,9 +3,6 @@ package mirsario.cameraoverhaul.common;
 import mirsario.cameraoverhaul.common.configuration.ConfigData;
 import mirsario.cameraoverhaul.common.systems.CameraSystem;
 import mirsario.cameraoverhaul.core.configuration.Configuration;
-import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
-import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
