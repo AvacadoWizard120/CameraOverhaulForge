c5ff7c5952e8f827a0ffba8edf274486d8bdb20e
bfybf <3247332139@qq.com>
2024-05-12 16:40:55 +0800

    port spectating crashes fix

--- src/main/java/mirsario/cameraoverhaul/core/callbacks/CameraUpdateCallback.java
+++ src/main/java/mirsario/cameraoverhaul/core/callbacks/CameraUpdateCallback.java
@@ -4,15 +4,16 @@ import mirsario.cameraoverhaul.core.events.Event;
 import mirsario.cameraoverhaul.core.events.EventHelper;
 import mirsario.cameraoverhaul.core.structures.Transform;
 import net.minecraft.client.Camera;
+import net.minecraft.world.entity.Entity;
 
 public interface CameraUpdateCallback {
     Event<CameraUpdateCallback> EVENT = EventHelper.CreateEvent(CameraUpdateCallback.class,
-            (listeners) -> (camera, transform, deltaTime) -> {
+            (listeners) -> (focusedEntity, camera, transform, deltaTime) -> {
                 for (CameraUpdateCallback listener : listeners) {
-                    listener.OnCameraUpdate(camera, transform, deltaTime);
+                    listener.OnCameraUpdate(focusedEntity, camera, transform, deltaTime);
                 }
             }
     );
 
-    void OnCameraUpdate(Camera camera, Transform cameraTransform, float deltaTime);
+    void OnCameraUpdate(Entity focusedEntity, Camera camera, Transform cameraTransform, float deltaTime);
 }
